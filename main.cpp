e22a9a0da61e22b0f182485b14e204d7e1747fb9
vmarni1998 <vmarni@mtu.edu>
2026-05-17 11:33:33 -0700

    Add success and failure email notifications

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Automated Build #3  is successful!Hope it works and I get to build such more ydss " << std::endl;
+    std::cout << "Automated Build #3  is successful! Hope it works and I get to build such more ydss " << std::endl;
     return 0;
 }
